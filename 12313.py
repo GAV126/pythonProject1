1795b0ef8234cc491f1608595d81eb7ad4191c1c
ykbaoom@gmail.com <Kai.Yao@redland.qld.gov.au>
2024-12-13 13:57:26 +1000

    numpy

--- 12313.py
+++ 12313.py
@@ -1,26 +1,3 @@
-filenames = ["cats.txt", "dogs.txt"]
-for filename in filenames:
-    print(f"\nReading file: {filename}")
-    try:
-       with open(filename) as f:
-            content = f.read()
-            print(content)
-    except FileNotFoundError:
-          print(f"Sorry, I can't find {filename}.")
-
-print("--------------------------------------------------")
-print("----")
-pets=[]
-pet = {"name":"huahua","owner":"kai"}
-pets.append(pet)
-pet = {"name":"wangcai","owner":"Julie"}
-pets.append(pet)
-pet = {"name":"huahua","owner":"kai"}
-pets.append(pet)
-pet = {"name":"miaomiao","owner":"Gavin"}
-pets.append(pet)
-print(pets)
-for pet in pets:
-    name = pet["name"]
-    owner = pet["owner"]
-    print(f"{name}'s owner is {owner}")  #print should be in1111
+import numpy as np
+arr1 = np.array([1,2,3])
+print(arr1)
